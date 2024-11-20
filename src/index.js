3af5ac001da3c303bf97604b98a5978b1b630468
Arturo Pérez <arjperez31@gmail.com>
2024-11-19 23:01:04 -0400

    first commit react-course

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
+ * Esta función devuelve el elemento root del Dom para el manejo del mismo
+ * y este contenido pasa a ser almacenado en la constante declarada
+ */
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+/*
+* Defimos el componente greeting(Saludar)
+? Importante: podemos definir los componentes tanto como arrow functions como también functions normales, dependerá completamente de cual de las versiones quieras usar.
+
+? Además de esto puedes retornar bloques de html tan complejos como sea necesario para definir tu componente
+*/
+
+const Greeting = () => {
+  function Add(x, y) {
+    return x + y;
+  }
+  return <h1>{Add(10, 30)}</h1>;
+};
+
+//* Renderizamos el componente Greeting dentro de una etiqueta html
+
+//? Forma que usaré
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//? Otra forma no tan conmún
+
+// root.render(
+//     <div>
+//         {Greeting()}
+//         {Greeting()}
+//         {Greeting()}
+//         {Greeting()}
+//     </div>
+// )
+
+//*Renderizamos el componente Greeting de manera directa
+
+// root.render(
+//     greeting()
+// )
